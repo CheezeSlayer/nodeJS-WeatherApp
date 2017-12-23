72bebfbe46db6e9a75659327b58c9e3fc7f0b2b7
Henry Chang <hchang1994@hotmail.com>
2017-12-23 13:07:24 -0800

    added api key variable

--- weather-app/weather/weather.js
+++ weather-app/weather/weather.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
+const API_KEY = '24431d0e56632af62a7e1891d23a0fd9'
+
 var getWeather = (lat, lng, callback) => {
   request({
-    url: `https://api.darksky.net/forecast/24431d0e56632af62a7e1891d23a0fd9/${lat},${lng}?units=si`,
+    url: `https://api.darksky.net/forecast/${API_KEY}/${lat},${lng}?units=si`,
     json: true
   }, (error, response, body) => {
       if(!error && response.statusCode === 200) {
