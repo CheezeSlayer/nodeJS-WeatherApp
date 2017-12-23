40434de173c3cae8f4f458b994b79f0a12818ab1
Henry Chang <hchang1994@hotmail.com>
2017-12-23 13:56:19 -0800

    added set units feature

--- weather-app/app.js
+++ weather-app/app.js
@@ -10,11 +10,25 @@ const argv = yargs //takes user input.  yargs allows for parsing arguments.
       alias: 'address',
       describe: 'Address to fetch weather for',
       string: true
+    },
+
+    u: {
+      alias: 'units',
+      describe: 'Unit Options: ca = Celsius, us = Fahrenheit.  Default: us',
+      choices: ['us', 'ca']
     }
   })
   .help()
   .alias('help', 'h')
   .argv;
+
+var options = {
+
+}
+
+if ( argv.units ) {
+  options.units = argv.u
+}
 
 //function responsible for all logic involved in handling user input address
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
@@ -23,7 +37,7 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   } else {
     console.log(results.address);
     //function responsible for handling geolocation output coordinates into DarkSky weather api
-    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+    weather.getWeather(results.latitude, results.longitude, argv.u, (errorMessage, weatherResults) => {
       if (errorMessage) {
         console.log(errorMessage);
       } else {
--- weather-app/weather/weather.js
+++ weather-app/weather/weather.js
@@ -1,10 +1,20 @@
 const request = require('request');
 
 const API_KEY = '24431d0e56632af62a7e1891d23a0fd9'
+const API_URL = `https://api.darksky.net/forecast/${API_KEY}`
 
-var getWeather = (lat, lng, callback) => {
+//`https://api.darksky.net/forecast/${API_KEY}/${lat},${lng}?units=us`
+
+var getWeather = (lat, lng, uOption, callback) => {
+
+  var url = API_URL + `/${lat},${lng}`
+
+  if ( uOption != undefined ){
+    url = url + `?units=${uOption}`
+  }
+
   request({
-    url: `https://api.darksky.net/forecast/${API_KEY}/${lat},${lng}?units=si`,
+    url: url,
     json: true
   }, (error, response, body) => {
       if(!error && response.statusCode === 200) {
@@ -13,7 +23,7 @@ var getWeather = (lat, lng, callback) => {
           apparentTemperature: body.currently.apparentTemperature
         });
       } else {
-        callback('Unable to fetch weather.');
+        callback(`Unable to fetch weather. statusCode: ${statusCode}`);
       }
   });
 }
